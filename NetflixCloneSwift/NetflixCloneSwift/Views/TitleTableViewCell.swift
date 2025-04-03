c10743d7cd0962b334d2ed34087b853cf0243515
enchufaocoding <enchufaocoding@gmail.com>
2025-04-03 16:33:32 +0200

    Finish Tab Upcoming
    Finish Tab Upcoming

---
+++ NetflixCloneSwift/NetflixCloneSwift/Views/TitleTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TitleTableViewCell.swift
+//  NetflixCloneSwift
+//
+//  Created by Jose Alberto Rosario Castillo on 3/4/25.
+//
+
+import UIKit
+
+class TitleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
